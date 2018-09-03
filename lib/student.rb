5f02fcbe64ecfad9ee2ca15f056331486ca1ef22
Katrina Pritchard <pritckat@gmail.com>
2018-09-03 17:01:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,24 +9,25 @@ class Student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = "SELECT * FROM students"
+
+    DB[conn:].execute(sql)
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
